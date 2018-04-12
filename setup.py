f2b02091e5ff5a7ef2f0d165f83fc4234d34bc7f
Alec Nikolas Reiter <alecreiter@gmail.com>
2018-04-11 20:29:12 -0400

    setup bumpversion

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('CHANGELOG', 'r') as f:
 if __name__ == "__main__":
     setup(
         name='flask-allows',
-        version='0.4',
+        version='0.4.0',
         author='Alec Nikolas Reiter',
         author_email='alecreiter@gmail.com',
         description='Impose authorization requirements on Flask routes',
